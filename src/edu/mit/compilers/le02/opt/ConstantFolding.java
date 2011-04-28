8e320d82ba24ce2976d82c94fc4454432f115ada
Shaunak Kishore <skishore@mit.edu>
2011-04-27 22:33:17 -0400

    ConstantFolding commutes constants with method calls

--- src/edu/mit/compilers/le02/opt/ConstantFolding.java
+++ src/edu/mit/compilers/le02/opt/ConstantFolding.java
@@ -1,8 +1,9 @@
 package edu.mit.compilers.le02.opt;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 import edu.mit.compilers.le02.DecafType;
 import edu.mit.compilers.le02.SourceLocation;
@@ -18,37 +19,52 @@ import edu.mit.compilers.le02.ast.SystemCallNode;
 import edu.mit.compilers.tools.CLI;
 
 public class ConstantFolding extends ASTNodeVisitor<Boolean> {
-  // This visitor searches the AST for lists of commuting addends or factors
-  // If any of the terms are CallNodes, they do not actually commute
-  // In these cases, the visitor returns a null list
-  private static class CommutingTerms extends ASTNodeVisitor<Boolean> {
-    private static CommutingTerms instance;
+  // This visitor searches a subtree of the AST for method calls
+  // and returns true if it finds any
+  // It is currently unused, but I'll include it in the file for now
+  private static class CheckForMethodCalls extends ASTNodeVisitor<Boolean> {
+    private static CheckForMethodCalls instance;
+    private static boolean foundMethodCall;
+
+    public static CheckForMethodCalls getInstance() {
+      if (instance == null) {
+        instance = new CheckForMethodCalls();
+      }
+      return instance;
+    }
+
+    public static Boolean check(ASTNode node) {
+      foundMethodCall = false;
+      node.accept(getInstance());
+      return foundMethodCall;
+    }
+
+    @Override
+    public Boolean visit(MethodCallNode node) {
+      foundMethodCall = true;
+      defaultBehavior(node);
+      return true;
+    }
+  }
+
+  // This visitor searches the AST for the list of all addends or factors
+  // of a sum or product expression
+  private static class ReconstructTerms extends ASTNodeVisitor<Boolean> {
+    private static ReconstructTerms instance;
     private static MathOp curOp;
     private static List<ExpressionNode> termsList;
-    private static boolean atTopLevel;
-    private static boolean foundMethodCall;
   
-    public static CommutingTerms getInstance() {
+    public static ReconstructTerms getInstance() {
       if (instance == null) {
-        instance = new CommutingTerms();
+        instance = new ReconstructTerms();
       }
       return instance;
     }
   
     public static List<ExpressionNode> getTerms(MathOpNode node) {
-      curOp = node.getOp();
-      if (curOp != MathOp.ADD || curOp != MathOp.MULTIPLY) {
-        return null;
-      }
-
       termsList = new ArrayList<ExpressionNode>();
-      atTopLevel = false;
-      foundMethodCall = false;
-
+      curOp = node.getOp();
       node.accept(getInstance());
-      if (foundMethodCall) {
-        return null;
-      }
       return termsList;
     }
 
@@ -59,31 +75,20 @@ public class ConstantFolding extends ASTNodeVisitor<Boolean> {
 
     @Override
     public Boolean visit(MathOpNode node) {
-      boolean oldAtTopLevel = atTopLevel;;
+      List<ExpressionNode> children =
+          new ArrayList<ExpressionNode>(
+          Arrays.asList(node.getLeft(), node.getRight()));
 
       for (ASTNode child : node.getChildren()) {
         ExpressionNode expr = (ExpressionNode)child;
 
-        if (oldAtTopLevel) {
-          if (continuesOp(expr)) {
-            atTopLevel = true;
-          } else {
-            termsList.add(expr);
-            atTopLevel = false;
-          }
+        if (continuesOp(expr)) {
+          expr.accept(this);
+        } else {
+          termsList.add(expr);
         }
-
-        expr.accept(this);
       }
 
-      atTopLevel = oldAtTopLevel;
-      return true;
-    }
-
-    @Override
-    public Boolean visit(MethodCallNode node) {
-      foundMethodCall = true;
-      defaultBehavior(node);
       return true;
     }
   }
@@ -103,15 +108,18 @@ public class ConstantFolding extends ASTNodeVisitor<Boolean> {
   }
 
   public Boolean visit(MathOpNode node) {
-    List<ExpressionNode> terms = CommutingTerms.getTerms(node);
-    if (terms == null) {
+    MathOp curOp = node.getOp();
+    if (curOp != MathOp.ADD && curOp != MathOp.MULTIPLY) {
+      // No folding is done on subtraction or division nodes
+      // so for now just recurse on the subtree
       defaultBehavior(node);
       return true;
     }
 
+    List<ExpressionNode> terms = ReconstructTerms.getTerms(node);
     List<IntNode> constants = new ArrayList<IntNode>();
-    MathOp curOp = node.getOp();
-    Iterator<ExpressionNode> iter = terms.iterator();
+
+    ListIterator<ExpressionNode> iter = terms.listIterator();
     ExpressionNode cur;
     while (iter.hasNext()) {
       cur = iter.next();
@@ -121,44 +129,48 @@ public class ConstantFolding extends ASTNodeVisitor<Boolean> {
       }
     }
 
-    if (constants.size() > 1) {
-      int eval = 1;
-      if (curOp == MathOp.ADD) {
-        eval = 0;
-      }
-
-      for (IntNode constant : constants) {
-        if (curOp == MathOp.ADD) {
-          eval += constant.getValue();
-        } else {
-          eval *= constant.getValue();
-        }
-      }
-
-      SourceLocation sl = new SourceLocation(CLI.getInputFilename(), -1, -1);
-      IntNode newConstant = new IntNode(sl, eval);
-
-      if (terms.isEmpty()) {
-        node.getParent().replaceChild(node, newConstant);
-      } else {
-        terms.add(newConstant);
-        iter = terms.iterator();
-
-        MathOpNode newNode = 
-            new MathOpNode(sl, iter.next(), iter.next(), curOp);
-        while (iter.hasNext()) {
-          newNode = new MathOpNode(sl, newNode, iter.next(), curOp);
-        }
-
-        node.getParent().replaceChild(node, newNode);
-        for (ExpressionNode child : terms) {
-          child.accept(this);
-        }
-      }
+    if (constants.size() < 2) {
+      defaultBehavior(node);
       return true;
     }
 
-    defaultBehavior(node);
+    int eval;
+    if (curOp == MathOp.ADD) {
+      eval = 0;
+    } else {
+      eval = 1;
+    }
+
+    for (IntNode constant : constants) {
+      if (curOp == MathOp.ADD) {
+        eval += constant.getValue();
+      } else {
+        eval *= constant.getValue();
+      }
+    }
+
+    SourceLocation sl = new SourceLocation(CLI.getInputFilename(),
+        constants.get(0).getSourceLoc().getLine(), -1);
+    IntNode newConstant = new IntNode(sl, eval);
+
+    if (terms.isEmpty()) {
+      node.getParent().replaceChild(node, newConstant);
+    } else {
+      terms.add(newConstant);
+      iter = terms.listIterator();
+
+      MathOpNode newNode = 
+          new MathOpNode(sl, iter.next(), iter.next(), curOp);
+      while (iter.hasNext()) {
+        newNode = new MathOpNode(sl, newNode, iter.next(), curOp);
+      }
+
+      node.getParent().replaceChild(node, newNode);
+      for (ExpressionNode child : terms) {
+        child.accept(this);
+      }
+    }
+
     return true;
   }
 }
